15be38d0fe81dd5c689036b2be3f58d1caffa3ab
Rafael Alberto Moreno Parra <ramsoftware@gmail.com>
2020-05-18 16:46:44 -0500

    Add files via upload

---
+++ Genetico03/Program.cs
@@ -0,0 +1,19 @@
+﻿/* Algoritmo genético: Operador cruce y mutación */
+using System;
+
+namespace Genetico03 {
+    class Program {
+        static void Main() {
+            string cadOriginal = "Estoy probando un algoritmo genético";
+
+            Poblacion objPoblacion = new Poblacion();
+            int TotalIndividuos = 2000;
+            int TotalCiclos = 100000;
+            string MejorIndiv = objPoblacion.Proceso(cadOriginal, TotalIndividuos, TotalCiclos);
+
+            //Muestra el individuo mejor adaptado
+            Console.WriteLine(MejorIndiv);
+            Console.ReadKey();
+        }
+    }
+}
